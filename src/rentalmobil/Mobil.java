d4a9ed431e99e745e942efccc077a8bebfeebb17
Karina Widya <karinawidya9999@gmail.com>
2018-12-14 11:35:19 +0700

    Mmebuat abstract class Mobil yaitu dengan atribut no_pol, merk, tahun, pembuatan, dan status mobil yang akan di override di class yang akan menjadi turunan class mobil

---
+++ src/rentalmobil/Mobil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalmobil;
+
+/**
+ *
+ * @author ASUS
+ */
+public abstract class Mobil {
+    public String no_pol;
+    public String merk;
+    public int tahun_pembuatan;
+    public String status;
+
+    public Mobil() {
+    }
+}
