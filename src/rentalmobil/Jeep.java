53851b2c2be75709f108b26ae5942c45e550cf83
Reina Trycha Dani <trycha67@gmail.com>
2018-12-14 12:34:23 +0700

    menambahkan atribut pada class Jeep

--- src/rentalmobil/Jeep.java
+++ src/rentalmobil/Jeep.java
@@ -9,6 +9,36 @@ package rentalmobil;
  *
  * @author ASUS
  */
-public class Jeep {
+public class Jeep extends Mobil implements Proses_Sewa{
+    //atribut untuk class Jeep
+    public int harga;
+    public int kuota;
+    public Jeep jip[];
     
+    
+
+    @Override
+    public void Sewa_Jeep(Penyewa p, Jeep jp) {
+        
+    }
+    
+    @Override
+    public void Sewa_Minibus(Penyewa p, Mini_Bus mb) {
+        
+    }
+
+    @Override
+    public void Sewa_Truk(Penyewa p, Truk tr) {
+        
+    }
+
+    @Override
+    public void Sewa_Sedan(Penyewa p, Sedan sd) {
+        
+    }
+
+    @Override
+    public void Sewa_Pickup(Penyewa p, Pickup pk) {
+        
+    }
 }
