3ff27c99e7192984debf427a2c42200aaaa2fc10
Reina Trycha Dani <trycha67@gmail.com>
2018-12-14 12:55:31 +0700

    menambahkan atribut untuk class Mini_Bus

--- src/rentalmobil/Mini_Bus.java
+++ src/rentalmobil/Mini_Bus.java
@@ -9,6 +9,36 @@ package rentalmobil;
  *
  * @author ASUS
  */
-class Mini_Bus {
+public class Mini_Bus extends Mobil implements Proses_Sewa{
+    //atribut untuk class Mini_Bus
+    public int harga;
+    public int kuota;
+    public Mini_Bus mini[];
+    
+    
+    @Override
+    public void Sewa_Minibus(Penyewa p, Mini_Bus mb) {
+        
+    }
+
+    @Override
+    public void Sewa_Truk(Penyewa p, Truk tr) {
+        
+    }
+
+    @Override
+    public void Sewa_Sedan(Penyewa p, Sedan sd) {
+        
+    }
+
+    @Override
+    public void Sewa_Pickup(Penyewa p, Pickup pk) {
+        
+    }
+
+    @Override
+    public void Sewa_Jeep(Penyewa p, Jeep jp) {
+        
+    }
     
 }
