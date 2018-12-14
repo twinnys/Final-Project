c1ac012ef0ad990a84e7662bb8d3dbaab7926524
Reina Trycha Dani <trycha67@gmail.com>
2018-12-14 13:05:02 +0700

    menambahkan method sewa_Minibus untuk menampilkan informasi penyewa dan informasi Minibusnya

--- src/rentalmobil/Mini_Bus.java
+++ src/rentalmobil/Mini_Bus.java
@@ -79,9 +79,29 @@ public class Mini_Bus extends Mobil implements Proses_Sewa{
         System.out.println("********************************");
     }
     
+    //untuk informasi sewa Minibus
     @Override
     public void Sewa_Minibus(Penyewa p, Mini_Bus mb) {
+        int kembali,tot;
+        tot = mb.harga * p.lama_sewa;
+        kembali = p.bayar - tot;
         
+        System.out.println(" ");
+        System.out.println(" Nama Penyewa : " + p.nama_penyewa);
+        System.out.println(" Alamat Penyewa : " + p.alamat);
+        System.out.println(" Nomor Telepon : " + p.no_telp);
+        System.out.println(" Tanggal Sewa : " + p.tgl_sewa);
+        System.out.println(" Lama Sewa : " + p.lama_sewa);
+        System.out.println("********************************");
+        System.out.println(" Jenis Mobil : Minibus");
+        System.out.println(" Nomor Polisi : " + mb.no_pol);
+        System.out.println(" Merk : " + mb.merk);
+        System.out.println(" Tahun Pembuatan : " + mb.tahun_pembuatan);
+        System.out.println(" Harga Sewa : " + mb.harga);
+        System.out.println("********************************");
+        System.out.println(" Bayar            :Rp" + p.bayar);
+        System.out.println(" Kembali          :Rp" + kembali);
+        System.out.println("********************************");
     }
 
     @Override
