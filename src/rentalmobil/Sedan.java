5a6e8d84472e635a811e8fc8d4228a76817d0c58
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-12-14 14:41:40 +0700

    menambahkan konstruktor pada class Sedan

--- src/rentalmobil/Sedan.java
+++ src/rentalmobil/Sedan.java
@@ -9,11 +9,24 @@ package rentalmobil;
  *
  * @author ASUS
  */
-public class Sedan extends implements Proses_Sewa{
+public class Sedan extends Mobil implements Proses_Sewa{
     
     public int harga;//atribut harga untuk menyimpan nilai harga
     public int kuota;//atribut kuota untuk menyimpan nilai kuota
     public Sedan sed[];//atribut untuk menyimpan jenis-jenis mobil sedan
+
+    //membuat konstruktor untuk class sedan
+    public Sedan() {
+    }
+
+    public Sedan(String no_pol, String merk, int tahun_pembuatan, int harga, int kuota) {
+        this.no_pol = no_pol;
+        this.merk = merk;
+        this.tahun_pembuatan = tahun_pembuatan;
+        this.status = status;
+        this.harga = harga;
+        this.kuota = kuota;
+    }
     
     
 }
