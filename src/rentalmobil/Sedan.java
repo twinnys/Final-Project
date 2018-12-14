bb3c57541d054c4b363e50a543a3be96300669d2
Nurcahya Nania A <nania.anabela22@gmail.com>
2018-12-14 14:48:14 +0700

    Menambahkan Override pada class Sedan

--- src/rentalmobil/Sedan.java
+++ src/rentalmobil/Sedan.java
@@ -27,6 +27,74 @@ public class Sedan extends Mobil implements Proses_Sewa{
         this.harga = harga;
         this.kuota = kuota;
     }
+
+    public void setKuota(int kuota) {
+        this.kuota = kuota;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public int getKuota() {
+        return kuota;
+    }
+
+    public String getNo_pol() {
+        return no_pol;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public int getTahun_pembuatan() {
+        return tahun_pembuatan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public void Sewa_Minibus(Penyewa p, Mini_Bus mb) {
+    }
+
+    @Override
+    public void Sewa_Truk(Penyewa p, Truk tr) {
+    }
+
+    @Override
+    public void Sewa_Pickup(Penyewa p, Pickup pk) {
+    }
+
+    @Override
+    public void Sewa_Jeep(Penyewa p, Jeep jp) {
+    }
     
-    
+    @Override
+    public void Sewa_Sedan(Penyewa p, Sedan sd) {
+        
+        int kembali,tot;
+        tot = sd.harga * p.lama_sewa;
+        kembali = p.bayar - tot;
+        
+        System.out.println(" ");
+        System.out.println(" Nama Penyewa : " + p.nama_penyewa);
+        System.out.println(" Alamat Penyewa : " + p.alamat);
+        System.out.println(" Nomor Telepon : " + p.no_telp);
+        System.out.println(" Tanggal Sewa : " + p.tgl_sewa);
+        System.out.println(" Lama Sewa : " + p.lama_sewa);
+        System.out.println("********************************");
+        System.out.println(" Jenis Mobil : Sedan");
+        System.out.println(" Nomor Polisi : " + sd.no_pol);
+        System.out.println(" Merk : " + sd.merk);
+        System.out.println(" Tahun Pembuatan : " + sd.tahun_pembuatan);
+        System.out.println(" Harga Sewa : " + sd.harga);
+        System.out.println("********************************");
+        System.out.println(" Bayar            :Rp" + p.bayar);
+        System.out.println(" Kembali          :Rp" + kembali);
+        System.out.println("********************************");
+        
+    }
 }
