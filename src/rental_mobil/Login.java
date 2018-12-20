259ca7257326fa9c6bd60ca7a65d0e9c9c2de7e3
Karina Widya <karinawidya9999@gmail.com>
2018-12-20 15:45:44 +0700

    Mengaktifkan tombol login dengan memisalkan username dan password admin

--- src/rental_mobil/Login.java
+++ src/rental_mobil/Login.java
@@ -102,7 +102,7 @@ public class Login extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    //mengaktifkan tombol login dengan memisalkan username dan pass "admin"
+    //mengaktifkan tombol login dengan username dan password dimisalkan "admin"
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         String username,password;
         username = txtUsername.getText();
